9e5602b605f04171c7bb4feb5eebf9d0b5f5883b
SimonRussia <git.streisand@gmail.com>
2018-02-01 11:30:16 +0300

    Update bucket.hpp

--- include/bucket.hpp
+++ include/bucket.hpp
@@ -7,8 +7,7 @@ template <typename T, typename RandomAccessIterator>
 void bucket_sort(RandomAccessIterator begin, RandomAccessIterator end, int rad) {
 
     // 1) Создаем корзину
-    int size = (end - begin);
-    std::vector<T> bucket[size];
+    std::vector<T> bucket[rad];
 
     // 2) Распределяем массив в различные корзины
     // (по n разряду) 256 -> [2]56
@@ -18,12 +17,12 @@ void bucket_sort(RandomAccessIterator begin, RandomAccessIterator end, int rad) 
     }
 
     // 3) Отдельно сортируем каждую корзину
-    for (int i = 0; i < size; i++)
+    for (int i = 0; i < rad; i++)
         std::sort(bucket[i].begin(), bucket[i].end());
 
     // 4) Сливаем отсортированные части в исходный массив
     // int index = 0;
-    for (int i = 0; i < size; i++) {
+    for (int i = 0; i < rad; i++) {
         for (int j = 0; j < bucket[i].size(); j++) {
             *begin = bucket[i][j];
             begin++;
